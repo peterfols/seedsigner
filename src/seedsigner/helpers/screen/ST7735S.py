4547c7b53b54ae966fed5e5212de3086dec344a2
pf <pf@pf.com>
2021-07-06 14:53:12 +0300

    more changes for scaling

--- src/seedsigner/helpers/screen/ST7735S.py
+++ src/seedsigner/helpers/screen/ST7735S.py
@@ -1,12 +1,10 @@
-from ST7789 import ST7789
 from _ST7735S import ST7735 as RefClass
+from ST7789 import ST7789
 from _ST7735S import BG_SPI_CS_FRONT
 
-
-class ST7735S(ST7789, RefClass):
+class ST7735S(RefClass, ST7789):
     def __init__(self, spi, rst=27, dc=25, bl=24):
         RefClass.__init__(self, port=spi, cs=BG_SPI_CS_FRONT, dc=dc, backlight=bl, rst=rst, rotation=0)
-        #ST7789.__init__(self, spi, rst, dc, bl)
 
     def command(self, cmd):
         RefClass.command(self, cmd)
@@ -22,9 +20,3 @@ class ST7735S(ST7789, RefClass):
 
     def SetWindows(self, Xstart, Ystart, Xend, Yend):
         RefClass.set_window(self, Xstart, Ystart, Xend, Yend)
-
-    def ShowImage(self, Image, Xstart, Ystart):
-        pass
-
-    def clear(self):
-        pass
